7ce40d73030bb1214542b04e9782a54e81629f16
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-04-02 16:59:04 -0700

    development tests

---
+++ JournalTests/DevelopmentTests.swift
@@ -0,0 +1,39 @@
+//
+//  DevelopmentTests.swift
+//  JournalTests
+//
+//  Created by Brandon Johns on 4/2/24.
+//
+
+import CoreData
+import XCTest
+
+@testable import Journal
+
+final class DevelopmentTests: BaseTestCase {
+
+    func testSampleDataCreationWorks() {
+        dataController.createSampleData()
+        
+        XCTAssertEqual(dataController.count(for: Topic.fetchRequest()), 5, "There should be 5 sample topics.")
+        XCTAssertEqual(dataController.count(for: Entry.fetchRequest()), 50, "There should be 50 sample entries.")
+    }
+    
+    func testDeleteAllClearsEverything() {
+        dataController.createSampleData()
+        dataController.deleteAll()
+
+        XCTAssertEqual(dataController.count(for: Topic.fetchRequest()), 0, "deleteAll() should leave 0 topics.")
+        XCTAssertEqual(dataController.count(for: Entry.fetchRequest()), 0, "deleteAll() should leave 0 entries.")
+    }
+    
+    func testExampleTagHasNoIssues() {
+        let topic = Topic.example
+        XCTAssertEqual(topic.entries?.count, 0, "The example topic should have 0 entries.")
+    }
+
+    func testExampleIssueIsHighPriority() {
+        let entry = Entry.example
+        XCTAssertEqual(entry.priority, 2, "The example entry should be high priority.")
+    }
+}
