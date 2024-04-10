87d803cd91a1cb899319cc99952b0f9fbacf1552
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-04-10 16:30:45 -0700

    UI Stuff

---
+++ Journal/MainView/AddingNewTopic.swift
@@ -0,0 +1,18 @@
+//
+//  AddingNewTopic.swift
+//  Journal
+//
+//  Created by Brandon Johns on 4/10/24.
+//
+
+import SwiftUI
+
+struct AddingNewTopic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingNewTopic()
+}
